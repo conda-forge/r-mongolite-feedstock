bebd6cb3b652366adc8c458335afc8f876565de8
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-28 10:32:56 -0800

    fix package

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-sed -i 's/PKG_CPPFLAGS=/PKG_CPPFLAGS=-Wno-implicit-function-declaration/' Makevars.in
+sed -i 's/PKG_CPPFLAGS=/PKG_CPPFLAGS=-Wno-implicit-function-declaration/' src/Makevars.in
 $R CMD INSTALL --build . ${R_ARGS}
